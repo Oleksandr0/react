55f9757aadd89ee36a793da38b71cb68d7c445c4
Oleksandr0 <iermolchev80@gmail.com>
2022-01-07 18:53:27 +0200

    intro lesson

---
+++ src/components/Simpson.js
@@ -0,0 +1,14 @@
+import './simpson.css'
+
+function Simpson(props){
+    let {name, surname, age, info, photo} = props;
+
+    return(
+        <div>
+            <h2>{name} {surname}; {age} yer old</h2>
+            <img src={photo} alt="name"/>
+            <p>{info}</p>
+        </div>
+    )
+}
+export default Simpson;
