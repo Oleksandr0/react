757536b1f910d21b0fae64adcbe79442521744f2
Oleksandr0 <iermolchev80@gmail.com>
2022-01-22 17:48:26 +0200

    complete hw1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Users from "./components/Users/Users";
 import Posts from "./components/Posts/Posts";
 import Comments from "./components/Comments/Comments";
-
+import './App.css';
 
 const App = () => {
 
