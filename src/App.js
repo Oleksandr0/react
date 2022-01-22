f313d0e3ee006b650eeb690a0cbdb2de1e409897
Oleksandr0 <iermolchev80@gmail.com>
2022-01-22 18:31:59 +0200

    complete hw1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Users from "./components/Users/Users";
 import Posts from "./components/Posts/Posts";
 import Comments from "./components/Comments/Comments";
 
+
 const App = () => {
 
     return (
         <div>
-            <div className={}>
+            <div className='usersPosts'>
                 <Users/>
                 <Posts/>
             </div>
